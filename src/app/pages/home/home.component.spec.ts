18c5229ed7a46d9b96be97fa5ccae440b69d33e5
SBC <salim.benchaabene@vneuron.com>
2025-04-04 03:11:49 +0100

    add Unit test

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,6 +1,12 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {HomeComponent} from './home.component';
+import {EvolutionService} from '../../services/evolution.service';
+import {Apollo} from 'apollo-angular';
+import {PokemonService} from '../../services/pokemon.service';
+import {Router} from '@angular/router';
+import {HttpClient, HttpHandler} from '@angular/common/http';
+import {HttpClientTestingModule, provideHttpClientTesting} from '@angular/common/http/testing';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +14,10 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
+      imports: [HomeComponent, HttpClientTestingModule],
+      providers: [
+        PokemonService
+      ]
     })
       .compileComponents();
 
--- src/app/pages/pokemon-detail/pokemon-detail.component.spec.ts
+++ src/app/pages/pokemon-detail/pokemon-detail.component.spec.ts
@@ -1,14 +1,28 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {PokemonDetailComponent} from './pokemon-detail.component';
+import {ActivatedRoute, convertToParamMap} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {PokemonService} from '../../services/pokemon.service';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {of} from 'rxjs';
 
 describe('PokemonDetailComponent', () => {
   let component: PokemonDetailComponent;
   let fixture: ComponentFixture<PokemonDetailComponent>;
 
+  const mockActivatedRoute = {
+    paramMap: of(convertToParamMap({ id: '1' }))
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PokemonDetailComponent]
+      imports: [PokemonDetailComponent,
+        HttpClientTestingModule],
+      providers: [
+        PokemonService,
+        { provide: ActivatedRoute, useValue: mockActivatedRoute }
+      ]
     })
       .compileComponents();
 
